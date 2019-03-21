fe7b993b1ecbd0dd829c63477fd86b740b479af3
Will Thiessen <willthiessen@gmail.com>
2019-03-21 15:57:04 -0400

    completed test

--- index.php
+++ index.php
@@ -30,42 +30,16 @@ $people = array(
         <th>Email</th>
         <th></th>
     </tr>
-    <tr>
-    <?php foreach ($people as $person) {
-        echo '<tr><td>'.$person['first_name'].'</td>';
-        echo '<td>'.$person['last_name'].'</td>';
-        echo '<td>'.$person['email'].'</td>';
-        echo '<td><button onClick="alert(\''.$person['first_name'].' '.$person['last_name'].' '.$person['email'].'\')">Alert</button></td></tr>';
-    }
-    ?>
+    <tbody>
+        <?php foreach ($people as $person): ?>
+            <tr>
+                <td><?php echo $person['first_name']; ?></td>
+                <td><?php echo $person['last_name']; ?></td>
+                <td><?php echo $person['email']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table> 
 
-<!-- Write a simple app which loops through the given $people array and displays it in a table.
-
-Add a button to each row which, when clicked, will alert the person's name and email.
-
-Place your code in index.php. Feel free to use the snippets below to get started.
-Snippets
-People -->
-
-<!-- 
-HTML Doctypes
-
-<!doctype html>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-	"http://www.w3.org/TR/html4/strict.dtd">
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	"http://www.w3.org/TR/html4/loose.dtd">
-
-Javascript libraries
-
-Mootools <script src="//ajax.googleapis.com/ajax/libs/mootools/1.5.2/mootools.min.js"></script>
-JQuery <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-AngularJS <script src="ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script> -->
-
+</body>
 </html>
